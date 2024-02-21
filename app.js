3650bedd3b86375c890a2c89d62e12956bdf1f8f
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-02-21 15:12:57 +0100

    Init commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const { v4 } = require('uuid');
+
+let CONTACTS = [];
+
+app.use(express.json());
+//GET
+app.get('/api/contacts', (req, res) => {
+  res.json(CONTACTS);
+});
+
+//POST
+app.post('/api/contacts', (req, res) => {
+  const contact = { ...req.body, id: v4(), marked: false };
+  CONTACTS.push(contact);
+  res.status(201).json(contact);
+});
+
+//PUT
+app.put('/api/contacts/:id', (req, res) => {
+  const idx = CONTACTS.findIndex((c) => c.id === req.params.id);
+  CONTACTS[idx] = req.body;
+  res.json(CONTACTS[idx]);
+});
+
+//DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+  CONTACTS = CONTACTS.filter((c) => c.id !== req.params.id);
+  res.json({ message: 'Contact removed' });
+});
+
+app.use(express.static(path.resolve(__dirname, 'client')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
